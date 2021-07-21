14559919a465e2122cabb3cc75778edbe0f40a4d
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-21 17:11:33 +0300

    Updates

---
+++ precache-manifest.96dab0934d4bdd8fc7aeec06dd52ff00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210ca8b0fc228f9dc5b1467b51a13af9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "553c2250e096732f92b0",
+    "url": "./static/css/main.5545e6ef.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553c2250e096732f92b0",
+    "url": "./static/js/main.f46d8fde.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
